e96ceea4c6318b7130934e33d6fb04a5a3361ee8
Marilyn Botheatoz <65403789+meryboth@users.noreply.github.com>
2024-06-08 12:27:37 -0300

    productDAO

--- Desafio Refactoring Custom Router/src/dao/daoFactory.js
+++ Desafio Refactoring Custom Router/src/dao/daoFactory.js
@@ -1,4 +1,5 @@
 import UserDAO from './models/userDAO.js';
+import ProductDAO from './models/productDAO.js';
 // Importar otros DAOs según sea necesario
 
 class DAOFactory {
@@ -6,6 +7,8 @@ class DAOFactory {
     switch (entity) {
       case 'user':
         return new UserDAO(dataSource);
+      case 'product':
+        return new ProductDAO(dataSource);
       // Añadir casos para otros DAOs aquí...
       default:
         throw new Error('Unknown entity type');
--- Desafio Refactoring Custom Router/src/services/products.services.js
+++ Desafio Refactoring Custom Router/src/services/products.services.js
@@ -1,4 +1,4 @@
-import ProductModel from '../models/product.model.js';
+/* import ProductModel from '../models/product.model.js';
 
 class ProductService {
   async getProducts({ limit = 10, page = 1, sort, query }) {
@@ -11,7 +11,7 @@ class ProductService {
       page,
       limit,
       sort: sortOptions,
-      lean: true, // Para obtener objetos JavaScript planos en lugar de documentos Mongoose
+      lean: true, 
     };
 
     return await ProductModel.paginate(queryOptions, options);
@@ -45,6 +45,50 @@ class ProductService {
 
   async deleteProduct(id) {
     const productoEliminado = await ProductModel.findByIdAndDelete(id);
+    if (!productoEliminado) {
+      throw new Error('Producto no encontrado');
+    }
+    return productoEliminado;
+  }
+}
+
+export default new ProductService();
+ */
+
+import DAOFactory from '../dao/daoFactory.js';
+import config from '../config/config.js';
+
+const dataSource = config.data_source || 'mongo'; // Utiliza mongo por defecto
+const productDAO = DAOFactory.getDAO('product', dataSource);
+
+class ProductService {
+  async getProducts({ limit = 10, page = 1, sort, query }) {
+    return await productDAO.getAllProducts({ limit, page, sort, query });
+  }
+
+  async getProductById(id) {
+    const producto = await productDAO.getProductById(id);
+    if (!producto) {
+      throw new Error('Producto no encontrado');
+    }
+    return producto;
+  }
+
+  async addProduct(productData) {
+    const nuevoProducto = await productDAO.createProduct(productData);
+    return nuevoProducto;
+  }
+
+  async updateProduct(id, productData) {
+    const productoActualizado = await productDAO.updateProduct(id, productData);
+    if (!productoActualizado) {
+      throw new Error('Producto no encontrado');
+    }
+    return productoActualizado;
+  }
+
+  async deleteProduct(id) {
+    const productoEliminado = await productDAO.deleteProduct(id);
     if (!productoEliminado) {
       throw new Error('Producto no encontrado');
     }
